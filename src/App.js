95f4a7d3f5779f242a7e13dc221ccbd77332e702
MugishaU <muwiragiye123@gmail.com>
2020-08-03 17:45:16 +0100

    create loop using map and create element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,66 @@
 import React from "react";
 class App extends React.Component {
   render() {
+    const music = [
+      {
+        artist: "J.Cole",
+        genre: "Rap",
+        album: "2014 Forest Hills Drive",
+        coverArt: "",
+        songs: ["No Role Modelz", "G.O.M.D.", "Apparently"],
+      },
+      {
+        artist: "Kendrick Lamar",
+        genre: "Rap",
+        album: "Good Kid Maad City",
+        coverArt: "",
+        songs: ["Don't kill my vibe", "Money Trees", "Poetic Justice"],
+      },
+      {
+        artist: "Burna Boy",
+        genre: "Afrobeats",
+        album: "African Giant",
+        coverArt: "",
+        songs: ["Gum Body", "African Giant", "Dangote"],
+      },
+      {
+        artist: "Lauryn Hill",
+        genre: "R&B",
+        album: "The Miseducation of Lauryn Hill",
+        coverArt: "",
+        songs: ["To Zion", "Nothing Even Matters", "Everything is Everything"],
+      },
+    ];
+    const hold = [];
+
     return (
       <>
         <h1>Hello World!</h1>
-        <p>Nice to meet you React</p>
+
+        <div>
+          {music.map((item) => {
+            return (
+              <Card
+                artist={item.artist}
+                genre={item.genre}
+                album={item.album}
+              ></Card>
+            );
+          })}
+        </div>
       </>
     );
   }
 }
 export default App;
+
+function Card(props) {
+  return (
+    <>
+      <h3>{props.artist}</h3>
+      <h4>Genre: {props.genre}</h4>
+      <h4>Album: {props.album}</h4>
+      <hr></hr>
+    </>
+  );
+}
