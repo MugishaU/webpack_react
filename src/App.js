e3a5b6e93149a32efafef4cb50c7c7d679439132
MugishaU <muwiragiye123@gmail.com>
2020-08-03 16:11:10 +0100

    setup basic rect app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <h1>Hello World!</h1>
+        <p>Nice to meet you React</p>
+      </main>
+    );
+  }
+}
+export default App;
