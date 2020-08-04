4fb1e533a3641034a216eed8723438cbb6e50409
MugishaU <muwiragiye123@gmail.com>
2020-08-04 16:13:51 +0100

    create component as class component

--- src/Components/card.js
+++ src/Components/card.js
@@ -1,22 +1,26 @@
 import React from "react";
 
-function Card(props) {
-  return (
-    <>
-      <h3>Artist: {props.artist}</h3>
-      <h4>Genre: {props.genre}</h4>
-      <h4>Album: {props.album}</h4>
-      <h4>Favourite Songs:</h4>
-      <ul>
-        {props.songs.map((song, idx) => {
-          return <li key={idx}>{song}</li>;
-        })}
-      </ul>
-      <button onClick={props.onClick}>Like</button>
-      <h4>{props.likeStatus}</h4>
-      <hr></hr>
-    </>
-  );
+class Card extends React.Component {
+  state = {
+    like: true,
+  };
+  render() {
+    return (
+      <>
+        <h3>Artist: {this.props.artist}</h3>
+        <h4>Genre: {this.props.genre}</h4>
+        <h4>Album: {this.props.album}</h4>
+        <h4>Favourite Songs:</h4>
+        <ul>
+          {this.props.songs.map((song, idx) => {
+            return <li key={idx}>{song}</li>;
+          })}
+        </ul>
+        <button onClick={this.props.onClick}>Like</button>
+        <h4>{this.props.likeStatus}</h4>
+        <hr></hr>
+      </>
+    );
+  }
 }
-
 export default Card;
